06d450cd3fc1ddfa2eab2d3bf4ad39013ee86286
Washington-Ksea <ksea.washington@gmail.com>
2019-07-10 13:45:57 +0900

    create repository

---
+++ .eslintrc.js
@@ -0,0 +1,94 @@
+module.exports = {
+    root: true,
+    env: {
+      browser: true,
+      node: true
+    },
+    parserOptions: {
+      ecmaVersion: 6,
+      ecmaFeatures: {
+        jsx: true
+      },
+      "sourceType": "module"
+    },
+    extends: [
+      'airbnb',
+      "eslint:recommended", //ESLintで基本的なルールチェック(更に細かく個別指定可)
+      'plugin:import/errors',
+      'plugin:import/warnings',
+      'plugin:prettier/recommended',
+      'prettier',
+      'prettier/@typescript-eslint',
+      'prettier/react'
+    ],
+    plugins: [
+      'jest',
+      'prettier',
+      'prefer-arrow',
+      'react',
+      'react-hooks'
+    ],
+    settings: {
+      'import/resolver': {
+        node: {
+          extensions: ['.js', 'jsx', '.ts', '.tsx']
+        }
+      },
+      react: {
+        version: 'detect'
+      }
+    },
+    // ここにカスタムルールを追加します。
+    rules: {
+      // eslint official
+      'newline-before-return': 'error',
+      'no-console': 'warn',
+      'require-yield': 'error',
+      //'no-unused-expressions': 'off',
+
+      // react
+      'react/jsx-filename-extension': [
+        'error',
+        {
+          extensions: ['jsx', 'tsx']
+        }
+      ],
+      'react/jsx-one-expression-per-line': 'off',
+      'react/jsx-uses-react': 'error',
+      'react/jsx-uses-vars': 'error',
+      'react/prop-types': 'off',
+      'react/prefer-stateless-function': 'off',
+
+      // react hooks
+      'react-hooks/rules-of-hooks': 'error',
+
+      // import
+
+      'import/extensions': [
+        'error',
+        'always',
+        {
+          js: 'never',
+          jsx: 'never',
+          ts: 'never',
+          tsx: 'never'
+        }
+      ],
+
+
+
+      'import/prefer-default-export': 'off',
+
+      // prettier
+      'prettier/prettier': [
+        'error', {
+          bracketSpacing: true,
+          printWidth: 80,
+          semi: true,
+          singleQuote: true,
+          trailingComma: 'all',
+          useTabs: false
+        }
+      ]
+    }
+}
---
+++ develop.md
@@ -0,0 +1,29 @@
+# .vscode
+
++ extension.json
+    拡張機能において@recommended:workspaceにて、インストールを推奨
+
++ eslint + prettier
+
+```javascript
+//npm i -D babel-eslint eslint eslint-config-prettier eslint-loader eslint-plugin-prettier prettier
+npm i -D eslint-config-prettier eslint-loader eslint-plugin-prettier prettier
+```
+
+[設定参考](https://makura3.github.io/Lint/eslint-Prettier/)
+
+---
+
+追加のパッケージ
+
+```javascript
+npm install eslint-plugin-react --save-dev
+
+npm install eslint-config-airbnb eslint-plugin-import eslint-plugin-jest eslint-plugin-jsx-a11y eslint-plugin-prefer-arrow eslint-plugin-react-hooks  --save-dev
+```
+
+# Sass
+
+```javascript
+yarn add node-sass
+```
