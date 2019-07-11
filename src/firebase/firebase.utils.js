2cafadd4c592f7757bd143dd09bfebf0affa07cf
Washington-Ksea <ksea.washington@gmail.com>
2019-07-11 16:30:57 +0900

    gitignore test

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,13 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyBcJKoVFloEy5xReyj_PDc-6i0ggSgQMRg',
+  apiKey: '',
   authDomain: 'crwn-db-1777b.firebaseapp.com',
   databaseURL: 'https://crwn-db-1777b.firebaseio.com',
   projectId: 'crwn-db-1777b',
   storageBucket: '',
   messagingSenderId: '986787964371',
-  appId: '1:986787964371:web:58c9c380a5ac1c08',
+  appId: '',
 };
 
 firebase.initializeApp(config);
